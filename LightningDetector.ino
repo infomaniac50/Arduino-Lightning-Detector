25f2a09aa47a0ed5159f94a0b3966b216ce725d0
Derek Chafin <infomaniac50@gmail.com>
2017-07-16 19:05:17 -0500

    Format Arduino code.

--- LightningDetector.ino
+++ LightningDetector.ino
@@ -23,65 +23,65 @@ bool toSend;
 
 void setup() {
 #if FASTADC
- // set prescale to 16
- sbi(ADCSRA,ADPS2) ;
- cbi(ADCSRA,ADPS1) ;
- cbi(ADCSRA,ADPS0) ;
+    // set prescale to 16
+    sbi(ADCSRA,ADPS2);
+    cbi(ADCSRA,ADPS1);
+    cbi(ADCSRA,ADPS0);
 #endif
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  pinMode(A4,INPUT);
-  Serial.println(micros());
-  
-  batchStarted=0;
-  batchEnded=0;
-  reading=0;
-  count=0;
-  maximum=0;
-  minimum=1023;
-  toSend=false;
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    pinMode(A4,INPUT);
+    Serial.println(micros());
+
+    batchStarted=0;
+    batchEnded=0;
+    reading=0;
+    count=0;
+    maximum=0;
+    minimum=1023;
+    toSend=false;
 }
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  reading = (analogRead(A4));
-  storage[count]=reading;
-  if ((!toSend)&&(count!=0)&&((reading>storage[count-1]+10)||(reading<storage[count-1]-10))){
-      toSend=true;
-  }
-  
-  count=count+1;
-  if ((count == 512) && (toSend))
-  {
-    count=0;
-    batchEnded = millis();
-    sendData();
-    batchStarted = millis();
-    
-  }
-  else if (count==512){
-    count=0;
-    batchEnded = millis();
-    //sendData();
-    batchStarted = millis();
-  
-  }
+    // put your main code here, to run repeatedly:
+    reading = (analogRead(A4));
+    storage[count]=reading;
+    if ((!toSend)&&(count!=0)&&((reading>storage[count-1]+10)||(reading<storage[count-1]-10))) {
+        toSend=true;
+    }
 
- 
+    count=count+1;
+    if ((count == 512) && (toSend))
+    {
+        count=0;
+        batchEnded = millis();
+        sendData();
+        batchStarted = millis();
+
+    }
+    else if (count==512) {
+        count=0;
+        batchEnded = millis();
+        //sendData();
+        batchStarted = millis();
+
+    }
+
+
 }
 
 void sendData()
 {
-  //Serial.print(">>>");
-  //Serial.println(batchStarted);
-  
-  for (int i=0;i<data;i++){
-    Serial.println(storage[i]);
-  }
-  //Serial.print("<<<");
-  //Serial.println(batchEnded);
-  //Serial.println("END");
-  
-  toSend=false;
+    //Serial.print(">>>");
+    //Serial.println(batchStarted);
+
+    for (int i=0; i<data; i++) {
+        Serial.println(storage[i]);
+    }
+    //Serial.print("<<<");
+    //Serial.println(batchEnded);
+    //Serial.println("END");
+
+    toSend=false;
 }
